ae0ea7da18f793eb2cb90729c8f6a6fc4a3b1320
Alibek <a.kazhibekov@mail.ru>
2019-12-04 13:25:18 +0200

    a user can fetch posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Resources\Post as PostResource;
+use App\Http\Resources\PostCollection;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -14,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return new PostCollection(Post::all());
     }
 
     /**
